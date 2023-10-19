457ab62b6bfb6b1ec192f4f11543caa494e72457
Tabish-yaseen <tabishmir36@gmail.com>
2023-10-19 15:01:05 +0530

    optimizing the leaderboard

--- controller/premium.js
+++ controller/premium.js
@@ -1,26 +1,28 @@
 const User=require('../model/user')
 const Expense=require('../model/expense')
+const { findAll } = require('../model/order')
+const sequelize = require('sequelize')
 
 
 exports.showLeaderBoard=async(req,res)=>{
     try{
-        let leadershipDetails=[]
-        
-        const users=await User.findAll()
-        for(let user of users){
-            const userid=user.id
-            const username=user.name
-            const expenses=await Expense.findAll({where:{userId:userid}})
-            let totalAmount=0
-            for(let expense of expenses){
-                totalAmount+=expense.amount
-               
+        const showLeaderBoardOfUsers=await User.findAll({
+            attributes:['id','name',[sequelize.fn('sum',sequelize.col('amount')),'totalcost']],
+            include:[{
+                model:Expense,
+                attributes:[]
+            }],
+            group:['id'],
+            order:[[sequelize.literal('totalcost'), 'DESC']]
 
-            }
-            leadershipDetails.push({userid:userid,name:username,totalcost:totalAmount})
-        }
-        leadershipDetails.sort((a, b) => b.totalcost - a.totalcost);
-        res.status(200).json(leadershipDetails)
+
+        })
+       
+        // console.log("titli",showLeaderBoardOfUsers)
+        res.status(200).json(showLeaderBoardOfUsers)
+
+       
+
         
         
 
